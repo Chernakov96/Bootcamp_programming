732190b4f39206a0f0620130840edbcccaab3e3b
Илья Чернаков <ichernakov@2A.local>
2022-11-01 15:03:23 +0300

    finished server-client

--- Client-Server/Client/Program.cs
+++ Client-Server/Client/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Это наш клиент");
+            OurClient ourClient = new OurClient();
+        }
+    }
+}
